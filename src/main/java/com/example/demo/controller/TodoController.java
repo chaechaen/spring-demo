d5609b61edf50b1fc7a562770ab24df9a051e424
aransword <lupin521@gmail.com>
2026-03-05 16:56:07 +0900

    feat: post implement

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,19 +1,27 @@
 package com.example.demo.controller;
+
+import com.example.demo.model.Todo;
 import com.example.demo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class TodoController {
-
     private final TodoService todoService;
 
     @GetMapping("/")
     public String getTodoList(Model model) {
-        model.addAttribute("todos", todoService.getTodoList());
-        return "todo-list";
+        model.addAttribute("list", todoService.getTodoList());
+        return "index.html";
+    }
+
+    @PostMapping("/add")
+    public String addTodo(Todo todo) {
+        todoService.addTodo(todo);
+        return "redirect:/";
     }
 }
